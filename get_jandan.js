0773466c18634b39d823d1cb4f2817922db92c4f
bowwowxx <jummykimo@gmail.com>
2016-08-08 14:28:11 +0800

    Update get_jandan.js

--- get_jandan.js
+++ get_jandan.js
@@ -19,7 +19,7 @@ function letItGo( cursor ) {
   request(  {
        url : url,
        headers : {
-           "User-Agent" : "User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.112 Safari/534.30"
+        "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.112 Safari/534.30'
        }
    }, function(error, response, body) {
         // console.log(body);
